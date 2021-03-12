b86532ab080956543d668387dd9f62d64ffe9fdd
acou12 <51276518+acou12@users.noreply.github.com>
2021-03-11 19:26:39 -0700

    Framework for more advanced stack modification functions

--- rpc.py
+++ rpc.py
@@ -1,6 +1,9 @@
 from math import *
+import traceback
+
 
 stack = []
+debug = True
 
 def main():
     while (True):
@@ -17,21 +20,27 @@ def evaluate():
     for token in split:
         token = token.lower()
         try: 
-            if (token in list(operations)):
-                stack.append(operations[token]())
+            if (token in list(operators)):
+                stack.append(operators[token]())
+            elif (token in list(special_functions)):
+                special_functions[token]()
             else:
                 stack.append(float(token))
         except:
             print("an error ocurred on the following token: " + token)
+            if (debug): 
+                traceback.print_exc()
             stack = backup_stack
             return
     print(stack[-1])
 
 
 def pop(n = -1):
-    return stack.pop(n) if len(stack) > 0 else 0
-    
-operations = {
+    return stack.pop(n) if len(stack) > -(1 + n) else 0
+
+
+
+operators = {
     '+': lambda: pop() + pop(),
     '-': lambda: pop(-2) - pop(),
     '--': lambda: - pop(),
@@ -46,7 +55,15 @@ operations = {
     'sqrt': lambda: sqrt(pop()),
     'q': lambda: sqrt(pop()),
     'e': lambda: pop(-2) * (10 ** pop()),
-    'ee': lambda: pop(-2) * (10 ** pop())
+    'ee': lambda: pop(-2) * (10 ** pop()),
+}
+
+special_functions = {
+    'swap': lambda: stack.append(pop(-2)),
+    'roll': lambda: stack.append(pop(int(-pop()-1))),
+    'drop': lambda: stack.pop(),
+    'dup': lambda: stack.append(stack[-1]),
+    'pick': lambda: stack.append(stack[int(-pop()-1)])
 }
 
 main()
