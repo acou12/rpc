3eea14f0371c2c8ff58008b7483eea3032a056ca
acou12 <51276518+acou12@users.noreply.github.com>
2022-02-15 20:54:06 -0700

    function syntax, variables

--- rpc.py
+++ rpc.py
@@ -3,27 +3,47 @@ import traceback
 
 
 stack = []
+variables = {}
 debug = True
 
 def main():
     while (True):
-        evaluate()
+        go()
 
-def evaluate():
+n = 0
+
+def evaluate(tokens):
+    tokens = [x for x in tokens if x != ""]
     global stack
     backup_stack = stack.copy() # in case there's an error
-    inp = input("RPC >>> ")
-    if inp == "":
-        print(stack)
-        return
-    split = inp.strip().split(' ')
-    for token in split:
+    making_expression = False
+    for token in tokens:
         token = token.lower()
         try: 
-            if (token in list(operators)):
+            if token[0] == '}':
+                making_expression = False
+                stack.append(expression)
+            elif making_expression:
+                expression += token + " "
+            elif token == '?':
+                if not pop():
+                    return 
+            elif token in list(operators):
                 stack.append(operators[token]())
-            elif (token in list(special_functions)):
+            elif token in list(special_functions):
                 special_functions[token]()
+            elif token[0] == '\\':
+                variables[token[1:]] = pop()
+            elif token[0] == '/':
+                if token[-1] == '!':
+                    evaluate(variables[token[1:-1]].split(' '))
+                else:
+                    stack.append(variables[token[1:]])
+            elif token[0] == '{':
+                making_expression = True
+                expression = ""
+            elif token[0] == '!':
+                evaluate(pop().split(' '))
             else:
                 stack.append(float(token))
         except:
@@ -32,13 +52,27 @@ def evaluate():
                 traceback.print_exc()
             stack = backup_stack
             return
-    print(stack[-1])
+
+def go():
+    global stack
+    inp = input("RPC >>> ")
+    if inp == "":
+        print(stack)
+        return
+    split = inp.strip().split(' ')
+    evaluate(split)
+    global n
+    if len(stack) > 0:
+        variables[str(n)] = stack[-1]
+        print("/" + str(n) + " = " + str(stack[-1]))
+        n += 1
 
 
 def pop(n = -1):
     return stack.pop(n) if len(stack) > -(1 + n) else 0
 
-
+def peek(n = -1):
+    return stack[-n] if len(stack) > -(n) else 0
 
 operators = {
     '+': lambda: pop() + pop(),
@@ -56,6 +90,7 @@ operators = {
     'q': lambda: sqrt(pop()),
     'e': lambda: pop(-2) * (10 ** pop()),
     'ee': lambda: pop(-2) * (10 ** pop()),
+    'len': lambda: len(stack)
 }
 
 special_functions = {
@@ -65,5 +100,17 @@ special_functions = {
     'dup': lambda: stack.append(stack[-1]),
     'pick': lambda: stack.append(stack[int(-pop()-1)])
 }
+
+# example function (factorial)
+evaluate(
+    "{ dup 1 - ? dup 1 - /fact! * } \\fact".split(' ')
+    # explaination:
+    # {                  begin function
+    # dup 1 -            push the top of the stack minus 1
+    # ?                  if it's zero, exit
+    # dup 1 - /fact! *   if not, push (n - 1)! * n
+    # }                  end function
+    # \fact             define the function as fact
+)
 
 main()
